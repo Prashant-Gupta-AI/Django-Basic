e99f04e30d97bd8b3aa62c84aff0bb5591621efd
Prashant-Gupta-AI <99531500+Prashant-Gupta-AI@users.noreply.github.com>
2022-07-06 12:24:04 +0530

    Simple apps

---
+++ Static Html render/myworld/members/views.py
@@ -0,0 +1,16 @@
+import imp
+from multiprocessing import context
+from re import template
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Members
+# Create your views here.
+
+def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
